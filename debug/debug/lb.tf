4d5eb90bf723a3ba52447e1ff18f17b4d597c442
benny <b1623546466@gmail.com>
2024-05-27 17:43:30 +0800

    save

--- debug/debug/lb.tf
+++ debug/debug/lb.tf
@@ -1,4 +1,11 @@
-data "huaweicloud_lb_loadbalancer" "prod_master" {}
+variable "lb_name" {
+    type = string
+    default = "prod_master"
+}
+
+data "huaweicloud_lb_loadbalancer" "prod_master" {
+    name = var.lb_name
+}
 
 output master_value {
   # Ids for multiple sets of EC2 instances, merged together
