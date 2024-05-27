0ddd8b8f59f5f64f56c79c53a8a70a1f4fe551c5
benny <b1623546466@gmail.com>
2024-05-27 16:23:05 +0800

    save

--- debug/terraform/k8s_master_rds.tf
+++ debug/terraform/k8s_master_rds.tf
@@ -1,18 +1,18 @@
 variable "postgreSQL_password" {}
 
-variable "availability_zone" {
-  type    = string
-  default = "ap-southeast-3a"
-}
+# variable "availability_zone" {
+#   type    = string
+#   default = "ap-southeast-3a"
+# }
 
 resource "huaweicloud_rds_instance" "k8s_pg" {
   name                = "k8s_pg"
   flavor              = "rds.pg.n1.large.2"
-  ha_replication_mode = "async"
+  # ha_replication_mode = "async"
   vpc_id              = huaweicloud_vpc.prod_vpc.id
   subnet_id           = huaweicloud_vpc_subnet.prod_private_db.id
   security_group_id   = huaweicloud_networking_secgroup.prod_private_db-default.id
-  availability_zone = [var.availability_zone]
+  availability_zone   = [data.huaweicloud_availability_zones.default.names[0]]
 
   db {
     type     = "PostgreSQL"
