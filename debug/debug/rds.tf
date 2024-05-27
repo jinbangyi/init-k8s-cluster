2faf172fa217c10f3695237f493a0c93682b6e18
benny <b1623546466@gmail.com>
2024-05-27 17:47:29 +0800

    save

--- debug/debug/rds.tf
+++ debug/debug/rds.tf
@@ -3,11 +3,11 @@ variable "rds_name" {
     default = "prod-k8s_pg"
 }
 
-data "huaweicloud_rds_instance" "k8s_pg" {
+data "huaweicloud_rds_instances" "k8s_pg" {
     name = var.rds_name
 }
 
 output master_value {
   # Ids for multiple sets of EC2 instances, merged together
-  value = data.huaweicloud_rds_instance.k8s_pg
+  value = data.huaweicloud_rds_instances.k8s_pg
 }
