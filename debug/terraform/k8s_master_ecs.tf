b104a662fc0b78f03c5c001783da7681418ef3e9
benny <b1623546466@gmail.com>
2024-05-27 20:08:29 +0800

    save

--- debug/terraform/k8s_master_ecs.tf
+++ debug/terraform/k8s_master_ecs.tf
@@ -68,7 +68,7 @@ resource "null_resource" "run_ansible" {
   triggers = {
     # public_ip
     ecs_ips = join(",", [for instance in huaweicloud_compute_instance.prod_master : instance.network.0.fixed_ip_v4])
-    hosts = join(",", [for instance in huaweicloud_compute_instance.prod_master : flavor_name + " " + instance.network.0.fixed_ip_v4])
+    hosts = join(",", [for instance in huaweicloud_compute_instance.prod_master : instance.flavor_name + " " + instance.network.0.fixed_ip_v4])
   }
 
   provisioner "local-exec" {
