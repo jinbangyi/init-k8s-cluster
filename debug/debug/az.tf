d777dbf5e1c09a33618043c76fd95ea0c60f04bf
benny <b1623546466@gmail.com>
2024-05-27 16:19:01 +0800

    save

--- debug/debug/az.tf
+++ debug/debug/az.tf
@@ -2,5 +2,5 @@ data "huaweicloud_availability_zones" "default" {}
 
 output master_value {
   # Ids for multiple sets of EC2 instances, merged together
-  value = huaweicloud_availability_zones.default
+  value = data.huaweicloud_availability_zones.default
 }
